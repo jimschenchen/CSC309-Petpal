7d6eb4a96bbed177312165e44fa12eb09d4182b8
bbrianh <brianhomanchon@gmail.com>
2023-12-04 05:17:59 -0500

    added material ui package, finished header and footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Logo = () => (
     <Link href="/dist/index.html" className="flex items-center hover:scale-105">
@@ -15,18 +16,77 @@ const Guest = () => (
     </div>
 );
 
-const Logged = ({username}) => {
-    <button type="button" class="flex items-center justify-end hover:scale-105"
-    onclick>
-        <div className="material-icons text-white px-1 sm:px-2" style="font-size: 40px">account_circle</div>
+const Logged = ({username, showMenu, setShowMenu}) => (
+    <button className="flex items-center justify-end hover:scale-105"
+    onClick={() => setShowMenu(!showMenu)}>
+        <AccountCircleIcon fontSize="large" style={{ color: 'white' }}/>
         <div className="text-white font-medium text-sm sm:text-lg py-1 sm:py-2 px-0 mx-1">
             {username}
         </div>
     </button>
-} 
+);
+
+const UserMenu = ({userType}) => {
+    if (userType === 'seeker') {
+        return (
+            <div class="bg-gray-50 rounded-lg z-50 absolute right-0 flex flex-col px-2">
+                <ul class="pt-2 pb-1 px-2 border-primary border-b-2">
+                    <li class="user-menu-item">
+                    <Link>My Applications</Link>
+                    </li>
+                    <li class="user-menu-item">
+                    <Link>Notification</Link>
+                    </li>
+                </ul>
+
+                <ul class="pt-1 pb-2 px-2 border-primary">
+                    <li class="user-menu-item">
+                        <Link>Edit profile</Link>
+                    </li>
+                    <li class="user-menu-item">
+                        <Link>Sign out</Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+    else if (userType === 'shelter'){
+        return (
+            <div 
+            class="bg-gray-50 border-primary rounded-lg z-50 absolute right-0 flex flex-col px-2">
+
+            <ul class="pt-2 pb-1 px-2 border-primary border-b-2">
+                <li class="user-menu-item">
+                <Link>Shelter details</Link>
+                </li>
+                <li class="user-menu-item">
+                    <Link>Manage Pets</Link>
+                </li>
+                <li class="user-menu-item">
+                    <Link>Manage Applications</Link>
+                </li>
+                <li class="user-menu-item">
+                <Link>Notification</Link>
+                </li>
+            </ul>
+
+            <ul class="pt-1 pb-2 px-2 border-primary">
+                <li class="user-menu-item">
+                    <Link>Edit profile</Link>
+                </li>
+                <li class="user-menu-item">
+                <Link>Sign out</Link>
+                </li>
+            </ul>
+            </div>
+        );
+    }
+}
 
 const Header = ({userType, username}) => {
-    const headerColor = (userType) => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const headerColor = () => {
         if (userType === 'seeker') {
             return "bg-seeker";
         }
@@ -45,9 +105,10 @@ const Header = ({userType, username}) => {
                 <Logo/>
                 {console.log(userType)}
                 {userType === 'guest' && <Guest/>}
-                {userType !== 'guest' && <Logged username={username}/>}
+                {userType !== 'guest' && <Logged username={username} showMenu={showMenu} setShowMenu={setShowMenu}/>}
             </div>
         </nav>
+        {userType !== 'guest' && showMenu && <UserMenu userType={userType}/>}
         </header>
     );
 }
