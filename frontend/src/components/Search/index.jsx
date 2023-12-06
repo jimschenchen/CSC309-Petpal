05396f977646e95a3a329832e7c92fd85986272b
bbrianh <brianhomanchon@gmail.com>
2023-12-06 03:36:52 -0500

    adjusted style

--- frontend/src/components/Search/index.jsx
+++ frontend/src/components/Search/index.jsx
@@ -140,14 +140,12 @@ const Search = () => {
 
           <div className="relative flex flex-auto h-10">
             <input
-              type="email"
               className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               placeholder=" "
               required
             />
             <button
               className="!absolute right-1 top-1 z-10 select-none rounded bg-primary py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-primary/20 transition-all hover:shadow-lg hover:shadow-primary/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none"
-              type="button"
               data-ripple-light="true"
             >
               Search
@@ -184,7 +182,7 @@ const Search = () => {
         <div id="main" className="flex-1 p-4 pt-0 overflow-y-auto">
           {/* <!-- ... main ... --> */}
 
-          <div id="cardsContainer" className="flex justify-center lg:justify-start flex-wrap">
+          <div id="cardsContainer" className="flex justify-center flex-wrap">
             {pets.map(pet => (
               <Card key={pet.id} item={pet} />
             ))}
