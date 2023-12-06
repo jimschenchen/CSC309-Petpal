7ee8d686d7919eca517527b7dd47f9b4c8e3f056
bbrianh <brianhomanchon@gmail.com>
2023-12-06 12:45:49 -0500

    fixed page frame navigation bug

--- frontend/src/components/PageFrame.jsx
+++ frontend/src/components/PageFrame.jsx
@@ -1,21 +1,28 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { getUser, isLogged } from "../utils/credential";
-import { Navigate } from "react-router";
+import { useNavigate } from "react-router";
 import { useState } from "react";
 
 const PageFrame = ({requiredLogin, requiredUserType, children}) => {
     const userInfo = getUser();
+    const navigate = useNavigate();
     const [log, setLog] = useState(isLogged());
 
     const logout = () => {
         setLog(false);
     }
 
+    if (requiredLogin && !isLogged()) {
+        navigate('/auth/login');
+    }
+
+    if (requiredUserType && userInfo.userType !== requiredUserType) {
+        navigate('/')
+    }
+
     return ( 
         <>
-        {requiredLogin && !isLogged() && <Navigate to="auth/login"/>}
-        {requiredUserType && userInfo.userType !== requiredUserType && <Navigate to='/'/>}
         <Header userType={userInfo.userType} username={userInfo.username} logout={logout}/>
         <main className="mt-0 bg-background">
             {children}
