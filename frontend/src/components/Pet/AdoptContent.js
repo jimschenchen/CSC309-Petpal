0bbbab50df7b6fa5dd73afedcd311f65e99cc2d9
bbrianh <brianhomanchon@gmail.com>
2023-12-07 01:16:24 -0500

    pet detail finished

--- frontend/src/components/Pet/AdoptContent.js
+++ frontend/src/components/Pet/AdoptContent.js
@@ -1,19 +1,41 @@
+import { Link } from "react-router-dom";
 
 const AdoptContent = ({ pet, onAdoptClick }) => {
+
+    const statusColor = (status) => {
+        switch (status){
+            case 'available':
+                return "text-green-600";
+            case 'pending':
+                return "text-yellow-600";
+            case 'withdrawn':
+                return "text-red-600";
+            case 'adopted':
+                return "text-red-600";
+            default:
+                return "text-primary";
+        }
+    }
+
     return (
-        <div className="adopt-content">
+        <div className="adopt-content detail-active-page lg:w-4/5 flex flex-col gap-2">
             {/* Shelter and adoption status */}
-            <div>
+            <div className="flex gap-2">
                 <div className="font-bold">Shelter:</div>
-                <a href="shelter-detail-public.html" className="text-primary hover:underline">Visit shelter page</a>
+                <Link to={'TODO'} className="text-primary hover:underline">
+                    Visit shelter page
+                </Link>
             </div>
-            <div className="mb-4 gap-3">
-                <label className="font-bold">Adoption Status:</label>
-                <p className="text-green-600">{pet.Status}</p>
+            <div className="flex gap-2">
+                <div className="font-bold">Adoption Status:</div>
+                <p className={statusColor(pet.status)}>{pet.status.charAt(0).toUpperCase() + pet.status.slice(1)}</p>
             </div>
-            <button onClick={onAdoptClick} className="bg-primary text-white hover:font-bold py-2 px-4 rounded">
+            {pet.status==='available' && <Link to={'TODO'} className="bg-primary w-fit text-white hover:font-bold py-2 px-4 rounded">
                 Adopt {pet.name}
-            </button>
+            </Link>}
+            {pet.status !== 'available' && 
+            <p className="text-red-600">This pet is currently not avaliable for adoption. Please check back for later updates.</p>}
+            
         </div>
     );
 };
--- frontend/src/pages/Pet_Detail/pet_details.jsx
+++ frontend/src/pages/Pet_Detail/pet_details.jsx
@@ -1,63 +1,60 @@
 import { useState } from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import InfoContent from '../../components/Pet/InfoContent';
 import DetailContent from '../../components/Pet/DetailContent';
 import AdoptContent from '../../components/Pet/AdoptContent';
+import { useParams } from 'react-router';
+import PageFrame from '../../components/PageFrame';
+import useFetchGet from '../../utils/useFetch';
+import { CircularProgress } from '@mui/material';
+
 
 const PetDetails = () => {
-    const [pets, setPets] = useState(
-
-        { id: 1, Name: 'Tom', Breed: 'Teddy', Age: '1 year', Size: 'small', Gender: 'Male', Status: 'Available' }
-
-    );
+    const { petId } = useParams();
 
     const [activeTab, setActiveTab] = useState('info');
 
-    const handleTabClick = (tab) => {
-        setActiveTab(tab);
-    };
+    const {data, isLoading, error} = useFetchGet(`pets/pet/${petId}`);
 
     const handleAdoptClick = () => {
 
     };
 
     return (
-        <div className='flex flex-col min-h-screen bg-background'>
-            <Header userType={"shelter"} username={'user'} />
+        <PageFrame>
+            {isLoading && <center className='mt-10'><CircularProgress color='inherit'/></center>}
+            {!isLoading && 
+            <div className="container mx-auto px-3 py-5 mt-8">
+                <div className="flex flex-col justify-center items-center">
+                    {/* <!-- Pet Photo --> */}
+                    {data.image &&
+                    <div className="mb-10 w-40 rounded-lg overflow-hidden scale-150">
+                        <img src={data.image}
+                            alt="Pet Image"/>
+                    </div>}
 
-
-            <main className="container mx-auto px-3 py-5">
-                <div className='flex flex-col justify-center '>
-                    {/* Pet Details Section */}
-                    {/* Pet Photo */}
-                    <div className="mb-4 w-40 rounded-lg overflow-hidden">
-                        <img src="\src\img\cat114.jpg" alt="Pet Image" />
-                    </div>
-
-                    {/* Pet Information Tabs */}
-                    <ul className="flex text-sm font-medium text-center text-gray-500 border-b border-gray-200 w-full">
+                    {/* <!-- Pet Information --> */}
+                    <ul className="flex  text-sm font-medium text-center text-gray-500 border-b border-gray-200 w-full lg:w-4/5">
                         <li className="mr-2">
-                            <button onClick={() => handleTabClick('info')} className={activeTab === 'info' ? 'detail-active-tab' : 'detail-inactive-tab'}>Info</button>
+                            <button onClick={() => setActiveTab('info')} 
+                            className={activeTab==='info'? 'detail-active-tab': 'detail-inactive-tab'}>Info</button>
                         </li>
                         <li className="mr-2">
-                            <button onClick={() => handleTabClick('detail')} className={activeTab === 'detail' ? 'detail-active-tab' : 'detail-inactive-tab'}>Detail</button>
+                            <button onClick={() => setActiveTab('detail')} 
+                            className={activeTab==='detail'? 'detail-active-tab': 'detail-inactive-tab'}>Detail</button>
                         </li>
                         <li className="mr-2">
-                            <button onClick={() => handleTabClick('adopt')} className={activeTab === 'adopt' ? 'detail-active-tab' : 'detail-inactive-tab'}>Adopt</button>
+                            <button onClick={() => setActiveTab('adopt')} 
+                            className={activeTab==='adopt'? 'detail-active-tab': 'detail-inactive-tab'}>Adopt</button>
                         </li>
                     </ul>
 
                     {/* Tab Content */}
-                    {activeTab === 'info' && <InfoContent pet={pets} />}
-                    {activeTab === 'detail' && <DetailContent pet={pets} />}
-                    {activeTab === 'adopt' && <AdoptContent pet={pets} onAdoptClick={handleAdoptClick} />}
+                    {activeTab === 'info' && !isLoading && <InfoContent pet={data} />}
+                    {activeTab === 'detail' && <DetailContent pet={data} />}
+                    {activeTab === 'adopt' && <AdoptContent pet={data} onAdoptClick={handleAdoptClick} />}                   
                 </div>
-
-            </main>
-
-            <Footer userType={"shelter"} />
-        </div>
+            </div>}
+        </PageFrame>
     );
 };
 
