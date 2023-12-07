ffb3200743c39d0f76ebb888a807b098931c2bbd
bbrianh <brianhomanchon@gmail.com>
2023-12-07 02:35:26 -0500

    fixed logout navigation bug

--- frontend/src/components/PageFrame.jsx
+++ frontend/src/components/PageFrame.jsx
@@ -11,6 +11,7 @@ const PageFrame = ({requiredLogin, requiredUserType, children}) => {
 
     const logout = () => {
         setLog(false);
+        navigate('/');
     }
 
     if (requiredLogin && !isLogged()) {
