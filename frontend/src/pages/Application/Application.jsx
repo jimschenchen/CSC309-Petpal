5e734c2dd97d110b895a5a54a407427ee9faf6ca
bbrianh <brianhomanchon@gmail.com>
2023-12-07 16:21:05 -0500

    added status change in application

--- frontend/src/pages/Application/Application.jsx
+++ frontend/src/pages/Application/Application.jsx
@@ -9,14 +9,6 @@ import NotFound from "../NotFound";
 import { CircularProgress } from "@mui/material";
 import { getUser } from "../../utils/credential";
 
-const TabBody = ({activeTab}) => {
-    if (activeTab === 'msg-tab') {
-        return <MessageBody/>
-    }
-    else {
-        return <ApplicationBody/>
-    }
-}
 
 const Application = () => {
     const [activeTab, setActiveTab] = useState('msg-tab');
@@ -27,7 +19,27 @@ const Application = () => {
     const [commentData, setCommentData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-    
+
+    const reloadApplication = () => {
+        setIsLoading(true);
+        fetch(
+            `https://petpal.api.jimschenchen.com/applications/application/${applicationId}/`, 
+            {method: "GET", 
+            headers:{'Content-Type': 'application/json', 'Authorization': `Bearer ${getUser().token}`}
+        })
+        .then(res => {
+            if (!res.ok) {throw Error(res.status)}
+            return res.json();
+        })
+        .then(data => {
+            setIsLoading(false);
+            setApplicationData(data);
+        })
+        .catch(err => {
+            setIsLoading(false);
+            setError(err.message);
+        })
+    }
 
     useEffect(() => {
         // fetch application data
@@ -49,24 +61,9 @@ const Application = () => {
         .then(res => res.json())
         .then(data => {
             setPetData(data);
-
-            // fetch comment data
-            return fetch(
-                `https://petpal.api.jimschenchen.com/comments/application/${applicationId}/comments/`,
-                {
-                    method: "GET", 
-                    headers:{
-                        'Content-Type': 'application/json', 
-                        'Authorization': `Bearer ${getUser().token}`
-                    }
-                })
-            }
-        )
-        .then(res => res.json())
-        .then(data => {
             setIsLoading(false);
-            setCommentData(data);
-        })
+        }
+        )
         .catch(err => {
             setIsLoading(false);
             setError(err.message);
@@ -87,7 +84,8 @@ const Application = () => {
                         <TabHeader activeTab={activeTab} setActiveTab={setActiveTab}/>
                         {activeTab === 'msg-tab' && 
                         <MessageBody commentData={commentData} applicationId={applicationId}/>}
-                        {activeTab === 'app-tab' && <ApplicationBody applicationData={applicationData} petData={petData}/>}
+                        {activeTab === 'app-tab' && <ApplicationBody applicationData={applicationData} 
+                        petData={petData} reloadApplication={reloadApplication} setIsLoading={setIsLoading}/>}
                     </>}
                     
                 </div>
