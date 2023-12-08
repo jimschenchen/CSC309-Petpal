19393ced1295973294a2dee248dffc1266bb3e08
jimschenchen <jimschenchen@gmail.com>
2023-12-08 16:18:35 -0500

    fix dependency

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -184,11 +184,12 @@ if USE_S3:
 else:
     # STATIC_URL = '/staticfiles/'
     STATIC_URL = "static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
