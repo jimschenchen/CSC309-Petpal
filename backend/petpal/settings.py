1b19850bfe969e7d285451e1d1efb869bac93522
jimschenchen <jimschenchen@gmail.com>
2023-12-08 16:27:20 -0500

    try staticfiles_build

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -165,6 +165,7 @@ USE_TZ = True
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
 if USE_S3:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
     # aws settings
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
