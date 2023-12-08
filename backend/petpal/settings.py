1caad7eb5b0d47be5edae5c5c285ca3ebcc9eae6
jimschenchen <jimschenchen@gmail.com>
2023-12-08 16:32:38 -0500

    commnet out s3 static settings

--- backend/petpal/settings.py
+++ backend/petpal/settings.py
@@ -165,7 +165,6 @@ USE_TZ = True
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
 if USE_S3:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
     # aws settings
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -175,9 +174,13 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
-    STATIC_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'petpal.storage_backends.StaticStorage'
+    # STATIC_LOCATION = 'static'
+    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    # STATICFILES_STORAGE = 'petpal.storage_backends.StaticStorage'
+
+    # using local static files
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
