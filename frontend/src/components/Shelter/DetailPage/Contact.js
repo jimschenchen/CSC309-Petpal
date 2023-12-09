b88a2642054a64ef426ed54dbed84bd8b5a32e2e
Hanli <hhanli.jiang@mail.utoronto.ca>
2023-12-09 13:47:48 -0500

    fix

--- frontend/src/components/Shelter/DetailPage/Contact.js
+++ frontend/src/components/Shelter/DetailPage/Contact.js
@@ -10,22 +10,17 @@ const Contact = ({ shelter }) => {
     const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${encodeAddressForURL(shelter?.address)}`;
   
     return (
-      <div className="bg-white p-6 rounded-lg shadow duration-300hover:bg-gray-100 hover:shadow-md hover:p-8 text-base">
-        <div className="text-left">
-          <strong>Location: </strong> 
-          <a href={googleMapsURL} target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">
-            {shelter?.address}
-          </a>
-        </div>
-        <div className="text-left">
-          <strong>Phone: </strong> {shelter?.phone_number}
-        </div>
-        <div className="text-left">
-        <strong>Email: </strong> 
-        <a href={`mailto:${encodedEmail}`} className="text-primary hover:underline">
-          {shelter?.email}
-        </a>
-      </div>
+      <div className="bg-white p-6 rounded-lg shadow duration-300 
+         hover:bg-gray-100 hover:shadow-md hover:p-8 text-base">
+            <div className="text-left">
+                <strong>Location:</strong> {shelter?.address}
+            </div>
+            <div className="text-left">
+                <strong>Phone:</strong> {shelter?.phone_number}
+            </div>
+            <div className="text-left">
+                <strong>Email:</strong> {shelter?.email}
+            </div>
       </div>
     );
   };
