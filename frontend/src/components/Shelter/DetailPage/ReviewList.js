2a9556fcbc79b7851049e7037b1b2e6f0475f94d
bbrianh <brianhomanchon@gmail.com>
2023-12-09 13:13:36 -0500

    remove console log

--- frontend/src/components/Shelter/DetailPage/ReviewList.js
+++ frontend/src/components/Shelter/DetailPage/ReviewList.js
@@ -7,7 +7,6 @@ import { CircularProgress } from "@mui/material";
 import { getUser } from "../../../utils/credential";
 
 const Review = ({review}) => {
-  console.log(review);
   return (
     <div className="p-4 text-base md:text-lg border-gray-200 border-b-2">
       {review.rating && <div><Rating value={review.rating} readOnly/></div>}
@@ -20,7 +19,9 @@ const Review = ({review}) => {
 
 const Reviews = ({shelter}) => {
   const [reviews, setReviews] = useState([]);
-  // const [hiddenReviews, setHiddenReviews] = useState([]);
+
+  const [avg_rating, setAvgRating] = useState(null);
+
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -33,12 +34,9 @@ const Reviews = ({shelter}) => {
   const [posted, setPosted] = useState(false);
   const [isPosting, setIsPosting] = useState(false);
 
-  // const {data, isLoading, error} = useFetchGet(
-  //   `comments/shelter/${shelter.id}/comments/?page_size=10&page=1`
-  // );
 
   useEffect(() => {
-    fetch(`https://petpal.api.jimschenchen.com/comments/shelter/${shelter.id}/comments`, {
+    fetch(`https://petpal.api.jimschenchen.com/comments/shelter/${shelter.id}/comments?page=1&page_size=10`, {
       method: "GET",
       headers: {
           'Content-Type': 'application/json',
@@ -57,6 +55,17 @@ const Reviews = ({shelter}) => {
         setHasMorePage(false);
       }
 
+      return fetch(`https://petpal.api.jimschenchen.com/accounts/shelters/${shelter.id}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${getUser().token}`
+        }
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      setAvgRating(data.average_rating)
       setIsLoading(false);
     });
   }, [posted]);
@@ -84,16 +93,6 @@ const Reviews = ({shelter}) => {
     });
   }
 
-  // const StarRating = ({index}) => {
-  //   return (
-  //     <button onClick={() => setRating(index)} className="text-yellow-300">
-  //     {rating >= index? 
-  //     <StarIcon fontSize="large" color="inherit"/>
-  //     :<StarBorderIcon fontSize="large" color="inherit"/>}
-  //   </button>
-  //   )
-  // }
-
   const handlePost = () => {
     if (message === '') {
       setError('Comment cannot be empty');
@@ -118,7 +117,7 @@ const Reviews = ({shelter}) => {
       if(res.ok) {
         setRating(null);
         setMessage('');
-        setPosted(true);
+        setPosted(prev => !prev);
         setIsPosting(false);
       }
     })
@@ -127,13 +126,23 @@ const Reviews = ({shelter}) => {
 
   return (
     <>
-    <div className="flex flex-col gap-3 p-4 border-gray-200 border-b-2">
+    <div className="flex flex-col md:flex-row w-full">
+    <div className="md:w-1/2 flex flex-col gap-2 p-4 border-gray-200 border-b-2
+    md:border-r-2">
+      <h1 className="text-lg text-left">Average rating</h1>
+      <div className="flex items-center justify-center gap-2">
+        <div >{Math.round(shelter.average_rating*10)/10}</div>
+      <div className="w-fit"><Rating value={shelter.average_rating} precision={0.1} size="large" readOnly/></div>
+      </div>
+    </div>
+
+    <div className="md:w-1/2 flex flex-col gap-3 p-4 border-gray-200 border-b-2">
       <h1 className="text-lg text-left">Add your review</h1>
-      <div className="w-full gap-3 flex-col flex justify-center
-      md:w-1/2 md:justify-start md:items-start">
+      <div className="w-full gap-3 flex-col flex justify-center">
       
       
       <div className="flex flex-col gap-3">
+      <div>
       <Rating 
       value={rating}
       size="large"
@@ -141,14 +150,15 @@ const Reviews = ({shelter}) => {
         setRating(newValue);
       }}/>
       </div>
-      <textarea defaultValue={''} value={message} placeholder="Leave your comment here" name="comment" id="comment" cols="30" 
+      
+      </div>
+      <textarea value={message} placeholder="Leave your comment here" name="comment" id="comment" cols="30" 
       className="border-2 border-black w-full p-2 rounded-lg"
-      onChange={(e) => {setMessage(e.target.value)}}/>
+      onChange={(e) => {setError(''); setMessage(e.target.value)}}/>
       {error && <p className="text-red-600">{error}</p>}
       <div className="flex justify-center">
         <button 
-        className="h-8 rounded font-sans hover:font-bold bg-shelter text-white py-2 w-full 
-        md:w-fit md:px-8"
+        className="h-8 rounded font-sans hover:font-bold bg-shelter text-white py-2 w-full"
         onClick={handlePost}>
           {isPosting? <CircularProgress color="inherit" size="1rem"/>: "Post"}
         </button>
@@ -156,18 +166,14 @@ const Reviews = ({shelter}) => {
       </div>
       
     </div>
+    </div>
+    
 
     {isLoading && 
     <div className="h-fit flex justify-center w-full overflow-hidden mt-3">
       <CircularProgress color="inherit"/></div>}
 
     {!isLoading && 
-    <>
-    <div className="flex flex-col gap-2 p-4 border-gray-200 border-b-2">
-      <h1 className="text-lg text-left">Average rating</h1>
-      <div><Rating value={shelter.average_rating} precision={0.1} size="large" readOnly/></div>
-    </div>
-
     <div className="h-screen">
     <InfiniteScroll
     dataLength={reviews.length}
@@ -181,8 +187,7 @@ const Reviews = ({shelter}) => {
         <Review key={review.id} review={review}/>
       ))}
     </InfiniteScroll>
-    </div>
-    </>}
+    </div>}
     
     
 
