9d5df5983d3f44ff9ae730c20387e05eb04e5933
Hanli <hhanli.jiang@mail.utoronto.ca>
2023-12-09 13:43:30 -0500

    contact fix

--- frontend/src/components/Shelter/DetailPage/Contact.js
+++ frontend/src/components/Shelter/DetailPage/Contact.js
@@ -10,18 +10,18 @@ const Contact = ({ shelter }) => {
     const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${encodeAddressForURL(shelter?.address)}`;
   
     return (
-      <div className="bg-white p-6 rounded-lg shadow duration-300 hover:bg-gray-100 hover:shadow-md hover:p-8">
-        <h2 className="text-2xl font-bold mb-4">Contact & Location</h2>
-        <div>
+      <div className="bg-white p-6 rounded-lg shadow duration-300 
+      hover:bg-gray-100 hover:shadow-md hover:p-8 text-base">
+        <div className="text-left">
           <strong>Location: </strong> 
           <a href={googleMapsURL} target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">
             {shelter?.address}
           </a>
         </div>
-        <div>
+        <div className="text-left">
           <strong>Phone: </strong> {shelter?.phone_number}
         </div>
-        <div>
+        <div className="text-left">
         <strong>Email: </strong> 
         <a href={`mailto:${encodedEmail}`} className="text-primary hover:underline">
           {shelter?.email}
