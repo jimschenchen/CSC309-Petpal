6eb19364db8377cc75acb3e581dac18d1e487ecd
Hanli <hhanli.jiang@mail.utoronto.ca>
2023-12-09 14:12:57 -0500

    contact update

--- frontend/src/components/Shelter/DetailPage/Contact.js
+++ frontend/src/components/Shelter/DetailPage/Contact.js
@@ -1,29 +1,35 @@
 const Contact = ({ shelter }) => {
-    // Function to encode the address for use in a URL
-    const encodeAddressForURL = (address) => {
-      return encodeURIComponent(address);
-    };
-
-    const encodedEmail = encodeURIComponent(shelter?.email);
-  
-    // Construct the Google Maps URL with the shelter's address
-    const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${encodeAddressForURL(shelter?.address)}`;
-  
-    return (
-        <div className="bg-white p-6 rounded-lg shadow duration-300 
-         hover:bg-gray-100 hover:shadow-md hover:p-8 text-base">
-            <div className="text-left">
-                <strong>Location:</strong> {shelter?.address}
-            </div>
-            <div className="text-left">
-                <strong>Phone:</strong> {shelter?.phone_number}
-            </div>
-            <div className="text-left">
-                <strong>Email:</strong> {shelter?.email}
-            </div>
-      </div>
-    );
+  // Function to encode the address for use in a URL
+  const encodeAddressForURL = (address) => {
+    return encodeURIComponent(address);
   };
-  
-  export default Contact;
-  
+
+  const encodedEmail = encodeURIComponent(shelter?.email);
+
+  // Construct the Google Maps URL with the shelter's address
+  const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${encodeAddressForURL(shelter?.address)}`;
+
+  return (
+    <div className="bg-white p-6 rounded-lg shadow duration-300 
+         hover:bg-gray-100 hover:shadow-md hover:p-8 text-base">
+      <div className="text-left">
+        <strong>Location:</strong>
+        <a href={googleMapsURL} target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">
+          {shelter?.address}
+        </a>
+      </div>
+      <div className="text-left">
+        <strong>Phone:</strong> {shelter?.phone_number}
+      </div>
+      <div className="text-left">
+        <strong>Email:</strong>
+        <a href={`mailto:${encodedEmail}`} className="text-primary hover:underline">
+          {shelter?.email}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
+
