9a433bfc9dbfaddbb6d0d184cdf0014017db1291
Hanli <hhanli.jiang@mail.utoronto.ca>
2023-12-09 11:31:22 -0500

    update

--- frontend/src/components/Shelter/DetailPage/Contact.js
+++ frontend/src/components/Shelter/DetailPage/Contact.js
@@ -1,21 +1,30 @@
-import { useState } from "react";
-
-const Contact = ({shelter}) => {
-
+const Contact = ({ shelter }) => {
+    // Function to encode the address for use in a URL
+    const encodeAddressForURL = (address) => {
+      return encodeURIComponent(address);
+    };
+  
+    // Construct the Google Maps URL with the shelter's address
+    const googleMapsURL = `https://www.google.com/maps/search/?api=1&query=${encodeAddressForURL(shelter?.address)}`;
+  
     return (
-        <div className="bg-white p-6 rounded-lg shadow duration-300 hover:bg-gray-100 hover:shadow-md hover:p-8">
-            <h2 className="text-2xl font-bold mb-4">Contact & Location</h2>
-            <div>
-                <strong>Location:</strong> {shelter?.address}
-            </div>
-            <div>
-                <strong>Phone:</strong> {shelter?.phone_number}
-            </div>
-            <div>
-                <strong>Email:</strong> {shelter?.email}
-            </div>
+      <div className="bg-white p-6 rounded-lg shadow duration-300 hover:bg-gray-100 hover:shadow-md hover:p-8">
+        <h2 className="text-2xl font-bold mb-4">Contact & Location</h2>
+        <div>
+          <strong>Location:</strong> 
+          <a href={googleMapsURL} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+            {shelter?.address}
+          </a>
         </div>
+        <div>
+          <strong>Phone:</strong> {shelter?.phone_number}
+        </div>
+        <div>
+          <strong>Email:</strong> {shelter?.email}
+        </div>
+      </div>
     );
-};
-
-export default Contact;
+  };
+  
+  export default Contact;
+  
